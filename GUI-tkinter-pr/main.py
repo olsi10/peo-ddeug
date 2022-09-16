3f17b1acb955741d569675112056f0a9858c9bf8
olsi10 <wangbampang2035@gmail.com>
2022-09-16 09:53:46 +0900

    Label(위치, text ="텍스트").pack() -> 첫번째 인자값(위치)에 보여줘라

--- GUI-tkinter-pr/main.py
+++ GUI-tkinter-pr/main.py
@@ -9,14 +9,20 @@ root.resizable(True, False)  # 크기 조정 가능 여부
 
 root.title("peo-ddeug")  # 창 제목
 
-path, image = None, None
+# path, image = None, None
 
 # 파일 불러오기
-path = filedialog.askopenfilename(filetypes=[(
-    'Image File', 'jpg'), ('Image File', 'png'), ('Image File', 'gif'), ('Image File', 'bmp')])
-path = path.replace('\\', '/')
+# path = filedialog.askopenfilename(filetypes=[(
+#     'Image File', 'jpg'), ('Image File', 'png'), ('Image File', 'gif'), ('Image File', 'bmp')])
+# path = path.replace('\\', '/')
 
-if path == '':
-    exit()
+# if path == '':
+#     exit()
 
-print(path)
+# print(path)
+
+
+# 텍스트 / Label(위치, text ="텍스트").pack() -> 첫번째 인자값(위치)에 보여줘라
+Label(root, text='퍼뜩퍼뜩').pack()
+
+root.mainloop()
