7fd3d7ceac9ade319137dc99870588b92f46a9d6
olsi10 <wangbampang2035@gmail.com>
2022-10-20 08:49:37 +0900

    출처 냄기기

--- main.py
+++ main.py
@@ -66,13 +66,19 @@ btnC = tkinter.Button(root, text='커스텀 알람', font=btn_font,
                       width=btn_width, height=btn_height)
 btnC.grid(row=0, column=1)
 
+# 코드에서 작성하신 것처럼 grid 와 pack 을 혼용하여서 쓰시면 안됩니다.
+# 마치 군대에서 이등병에게 소대장이 "야, 여기 청소해" 를 함과 동시에
+# 분대장인 병장이 "야, 청소 하지마" 를 얘기하는 것과 같습니다.
+# 이등병은 어떻게 해야 할지 혼란스러워지겠죠..
+# ### 인프런 나도코딩 댓글 출처
+
 btnN.place(x=300, y=100)
 btnC.place(x=400, y=100)
 
 # type 의 객체를 생성하고 Button있지만 즉시 grid메서드를 호출하여 를 반환합니다
 # None. 따라서 pButton가 할당 None되고 이것이 다음 행이 실패하는 이유입니다.
 # 즉, 먼저 버튼을 생성하고 에 할당 pButton한 다음 그 위에 작업을 수행합니다.
-# Nonetype 에러나는 이유 : 스택오버플로우 출처
+##### Nonetype 에러나는 이유 : 스택오버플로우 출처
 
 btnN.config(command=click)
 
--- normal.py
+++ normal.py
@@ -28,13 +28,13 @@ from tkinter import *
 from tkinter import filedialog
 import tkinter.font
 from tokenize import group
-from turtle import bgcolor
+from turtle import bgcolor, width
 
 
 root = Tk()
 root.geometry("800x600")
 
-root.title("peo-ddeug-NORMAL")  # 창 제목
+root.title("peo-ddeug")  # 창 제목
 
 
 def click():
@@ -42,16 +42,40 @@ def click():
     import main
 
 
-title_font = tkinter.font.Font(family="메이플스토리", size=30)
-btn_font = tkinter.font.Font(family="메이플스토리", size=10)
+title_font = tkinter.font.Font(family="함초롬바탕", size=30)
+btn_font = tkinter.font.Font(family="함초롬바탕", size=10)
 
 # 텍스트 / Label(위치, text ="텍스트").pack() -> 첫번째 인자값(위치)에 보여줘라
-title = tkinter.Label(root, text='퍼뜩퍼뜩 다음 페이지!!', font=title_font)
+title = tkinter.Label(root, text='퍼뜩퍼뜩', font=title_font, width=10)
 
-btnBack = tkinter.Button(root, text='다시 메인으로!',
-                         font=btn_font, width=10, height=3)
-btnBack.grid(row=0, column=0)
+title.place(x=280, y=0)
 
-btnBack.config(command=click)
+# 버튼 / grid, frame
+btn_width, btn_height = 10, 3
+
+frame = tkinter.Frame(root, bg='#80c1ff', bd=5)
+# frame.place(x=200, y=300)
+
+# # 버튼 컴포넌트 생성
+# Button(위치, text='텍스트', width=너비, height=높이).pack()
+btnN = tkinter.Button(root, text='다시 메인으로', font=btn_font,
+                      width=btn_width, height=btn_height)
+btnN.grid(row=0, column=0)
+
+btnC = tkinter.Button(root, text='커스텀 알람', font=btn_font,
+                      width=btn_width, height=btn_height)
+btnC.grid(row=0, column=1)
+
+btnN.place(x=300, y=100)
+btnC.place(x=400, y=100)
+
+# type 의 객체를 생성하고 Button있지만 즉시 grid메서드를 호출하여 를 반환합니다
+# None. 따라서 pButton가 할당 None되고 이것이 다음 행이 실패하는 이유입니다.
+# 즉, 먼저 버튼을 생성하고 에 할당 pButton한 다음 그 위에 작업을 수행합니다.
+#### Nonetype 에러나는 이유 : 스택오버플로우 출처
+
+btnN.config(command=click)
+
+# frame.pack()
 
 root.mainloop()
